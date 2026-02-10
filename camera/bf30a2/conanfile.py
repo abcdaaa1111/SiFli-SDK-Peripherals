b33bb148805c634252e6983eaff4a5cdc234bea1
jingle_xie <xiebowen1@outlook.com>
2026-02-10 11:11:44 +0800

    Update bf30a2 driver conanfile (#3)
    * Update bf30a2 driver conanfile
    
    * Update bf30a2 driver conanfile
    
    * Update bf30a2 driver conanfile url

--- camera/bf30a2/conanfile.py
+++ camera/bf30a2/conanfile.py
@@ -1,23 +1,23 @@
 from conan import ConanFile
 
 class Bf30A2_DriverRecipe(ConanFile):
-    name = "bf30a2_driver"
+    name = "bf30a2"
     version = "1.1.0"
 
     license = "Apache-2.0"
     user = "jingle_xie"
     author = "jingle_xie"
-    url = "<Package recipe repository url here, for issues about the package>"
-    homepage = "<Package homepage here>"
-    description = "<Description of hello package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    url = "https://packages.sifli.com/zh/packages/jingle_xie/bf30a2"
+    homepage = "https://packages.sifli.com/zh/packages/jingle_xie/bf30a2"
+    description = "The BF30A2 SPI camera is equipped with a compliant device driver implementation."
+    topics = ("driver", "camera", "bf30a2")
 
     support_sdk_version = "^2.4"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "*"
 
-    python_requires = "sf-pkg-base/[^1.0]@sifli"
+    python_requires = "sf-pkg-base/[^1.1.0]@sifli"
     python_requires_extend = "sf-pkg-base.SourceOnlyBase"
 
     def requirements(self):
